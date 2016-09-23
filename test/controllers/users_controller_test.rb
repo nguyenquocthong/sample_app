f25b6c3c9ab8d7aa1c13d880d4e89ea261b480b9
nguyenquocthong <quocthong9592@gmail.com>
2016-09-23 10:21:13 +0700

    Finish layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get users_new_url
+    assert_response :success
+    get signup_path
+  end
+end
