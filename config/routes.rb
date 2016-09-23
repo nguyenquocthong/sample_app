d9c608f37069bbc058560c126f7d58db31cb1508
nguyenquocthong <quocthong9592@gmail.com>
2016-09-23 10:21:13 +0700

    Add a full title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root "static_pages#home"
+  
+  get "static_pages/contact"
+
+  get "static_pages/about"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
 end
