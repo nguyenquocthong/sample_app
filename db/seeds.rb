a8dc1c9d086a6901306438ee72f5a4137ca0a0be
nguyenquocthong <quocthong9592@gmail.com>
2016-10-05 15:49:24 +0700

    Add account activation-Chuong9

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 User.create!(name: "Quoc Thong", email: "quocthong9592@gmail.com",
-  password: "789987", password_confirmation: "789987", admin: true)
+  password: "789987", password_confirmation: "789987", admin: true,
+  activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name, email: email, password: password,
-    password_confirmation: password)
+  User.create!(name: name, email: email,password: password,
+    password_confirmation: password, activated: true,
+    activated_at: Time.zone.now)
 end
