2e175957d07b980cd68f21e99746c3f6f1d32f53
nguyenquocthong <quocthong9592@gmail.com>
2016-10-11 08:13:52 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,11 @@ class UsersController < ApplicationController
 
   def show
     @microposts = @user.microposts.paginate page: params[:page]
+    @relationship = if current_user.following? @user
+      current_user.active_relationships.find_by followed_id: @user.id
+    else
+      current_user.active_relationships.build
+    end
   end
 
   def new
@@ -44,7 +49,7 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-  
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,7 +6,8 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
   validate  :picture_size
-  
+  scope :following_ids, -> (user_id, following_ids) {where(
+    "user_id = ? OR user_id IN (?)", user_id, following_ids)}
   private
   def picture_size
     if picture.size > 5.megabytes
